dba1764c57cd3f90d1c90aceda0579ad646f9d95
mythdra <nguyencan878@gmail.com>
2023-08-24 02:52:02 +0700

    feature/test-rebase

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/can/fvm/versions/3.10.3"
-export "FLUTTER_APPLICATION_PATH=/Users/can/dong/atten"
+export "FLUTTER_APPLICATION_PATH=/Users/can/dong/FaceRecognitionAuth"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
